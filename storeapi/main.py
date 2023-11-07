38ff9510c0e8160b61c4b9a0f9c2b3ad155c4e75
David Casanovas Osorio <dcasanovaso@ioc.cat>
2023-11-07 15:34:33 +0100

    12

---
+++ storeapi/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from storeapi.models.post import UserPost, UserPostIn
+
+app = FastAPI()
+
+post_table = {}
+
+@app.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+@app.get("/posts", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
---
+++ storeapi/models/post.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class UserPostIn(BaseModel):
+    body: str
+
+class UserPost(UserPostIn):
+    id: int
