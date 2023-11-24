07274c3c60fc332a236e48c9d67fe7fa659d89df
planetacomputer <dcasanovaso@ioc.cat>
2023-11-24 20:38:06 +0100

    24

--- storeapi/config.py
+++ storeapi/config.py
@@ -2,6 +2,7 @@ from typing import Optional
 from pydantic import BaseSettings
 
 class BaseConfig(BaseSettings):
+    ENV_STATE: Optional[str] = None
     class Config:
         """Config for base settings."""
         env_file: str = ".env"
@@ -9,3 +10,17 @@ class BaseConfig(BaseSettings):
 class GlobalConfig(BaseConfig):
     DATABASE_URL: Optional[str] = None
     DB_FORCE_ROLL_BACK: bool = False
+
+class DevConfig(GlobalConfig):
+    class Config:
+        env_prefix: str = "DEV_"
+
+class ProdConfig(GlobalConfig):
+    class Config:
+        env_prefix: str = "PROD_"
+
+class TestConfig(GlobalConfig):
+    DATABASE_URL: str = "sqlite:///test.db"
+    DB_FORCE_ROLL_BACK: bool = True
+    class Config:
+        env_prefix: str = "TEST_"
